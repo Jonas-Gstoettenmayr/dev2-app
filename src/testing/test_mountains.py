c59c79856efb4ae2a92862e72f9a463a11763e22
Jonas-Gstoettenmayr <s2410929009@fhooe.at>
2025-05-23 10:08:13 +0200

    Add test job to pipeline

---
+++ src/testing/test_mountains.py
@@ -0,0 +1,11 @@
+import pytest
+from dev2il_devops_app.mountains import highest_mountain
+
+def test_highest_Mountain():
+    mountains = [
+        {'name': 'Spitzmauer', 'height': 2446},
+        {'name': 'Gr. Priel', 'height': 2515},
+        {'name': 'Schermberg', 'height': 2396}
+    ]
+    
+    assert highest_mountain(mountains) == mountains[1]
